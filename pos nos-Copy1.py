453bd11f34726a1e304b3608c4eb4c36180fecf5
urvi0604 <136330540+urvi0604@users.noreply.github.com>
2023-06-12 16:51:30 +0530

    Coding loops task 2
    This contains printing positive numbers in a range

---
+++ pos nos-Copy1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#to print positive numbers in a range 
+#Input 1
+list1 = (12,-7,5,64,-14)
+for num in list1:
+    if num>0:
+        print(num)
+        #output
+
+
+# In[12]:
+
+
+#to print positive numbers in a range 
+#Input2
+list2 = (12,14,-95,3)
+for num in list2:
+    if num>0:
+        print(num)
+        #output2
+
+
+# In[ ]:
+
+
+
+
